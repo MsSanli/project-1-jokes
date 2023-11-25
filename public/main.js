c2c187844d8b7af20335cee08678d5e0db91da9f
Maria Sanli <marisanli.93@gmail.com>
2023-11-24 21:04:14 -0500

    did the whole exercise on main

--- public/main.js
+++ public/main.js
@@ -2,21 +2,38 @@
 // import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import getJoke from '../api/promises';
+import renderToDom from '../utils/sample_data/renderToDom';
 
 const init = () => {
+  let showSetup = true;
+
   document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
+    <button class="btn btn-danger" id="click-me">Get a Joke!</button><br />
+    <div id='joke-setup'></div>
+    <div id='joke-punchline'></div>
   `;
   console.warn('YOU ARE UP AND RUNNING!');
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
+  const getPunchText = () => {
+    const button = document.querySelector('#click-me');
+    button.textContent = showSetup ? 'Get Another Joke' : 'Get Punchline';
+  };
+
+  document.querySelector('#click-me').addEventListener('click', () => {
+    getJoke().then((joke) => {
+      if (showSetup) {
+        renderToDom('#joke-setup', joke.setup);
+      } else {
+        renderToDom('#joke-punchline', joke.delivery);
+      }
+      //! negates showSetup
+      showSetup = !showSetup;
+
+      // change text on second click
+      getPunchText();
+    });
+  });
 
   // USE WITH FIREBASE AUTH
   // ViewDirectorBasedOnUserAuthStatus();
